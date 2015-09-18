afaaddab429bae0c63df138b5e8cf0b8e00004c7
Daniel Rydén <daniel.ryden@onposition.com>
2015-09-18 13:14:43 +0200

    appcs

--- source/Tarro/Tarro/Application.cs
+++ source/Tarro/Tarro/Application.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Security.Policy;
 using Tarro.Logging;
 
 namespace Tarro
 {
+    enum RunMode
+    {
+        AppDomain, Process
+    }
     internal class Application : IDisposable
     {
+
         private readonly ILog log = LogFactory.GetLogger<Application>();
         private readonly string name;
         private readonly string pathToApp;
@@ -14,14 +20,17 @@ namespace Tarro
         private readonly AppWatcher watcher;
         private readonly AppCopy appCopy;
         private Process process;
-
+        private AppDomain domain;
+        private readonly RunMode runMode;
         private readonly string cachePath = "appCache";
         public Application(string name, string pathToApp, string executable)
         {
             this.name = name;
             this.pathToApp = pathToApp;
             this.executable = executable;
-            appCopy =  new AppCopy(cachePath,pathToApp,executable);
+            this.runMode = RunMode.AppDomain;
+
+            appCopy = new AppCopy(cachePath, pathToApp, executable);
             watcher = new AppWatcher(pathToApp);
             watcher.AppChanged += (o, e) =>
             {
@@ -38,34 +47,25 @@ namespace Tarro
             try
             {
 
-                lock (processLock)
+                lock (appLock)
                 {
-                    if(process!=null) //Should be null if stopped
+                    if (RuntimeIsActive()) //Should be null if stopped
                         return;
                     log.Info("Starting application ({0})", name);
                     appCopy.ShadowCopy();
-                    var setup = CreateSetup();
-                    process = new Process();
-                    process.StartInfo = setup;
-                    process.EnableRaisingEvents = true;
-                    process.Exited += process_Exited;
-
-                    process.Start();
-
-
-                    if (Environment.UserInteractive)
+                    switch (runMode)
                     {
-                        process.ErrorDataReceived += (sendingProcess, errorLine) => log.Error(string.Format("[{0}] {1}", process.ProcessName, errorLine.Data));
-                        process.OutputDataReceived += (sendingProcess, dataLine) => log.Info(string.Format("[{0}] {1}", process.ProcessName, dataLine.Data));
-
-                        process.BeginErrorReadLine();
-                        process.BeginOutputReadLine();
-
-
-                    } 
+                        case RunMode.AppDomain:
+                            StartAppdomain();
+                            break;
+                        case RunMode.Process:
+                            StartProcess();
+                            break;
+                        default:
+                            throw new InvalidOperationException("Unknown runmode");
+                    }
                     log.Info("Application started ({0})", name);
                 }
-
             }
             catch (Exception ex)
             {
@@ -73,35 +73,98 @@ namespace Tarro
             }
         }
 
-        void process_Exited(object sender, EventArgs e)
+        private bool RuntimeIsActive()
         {
-            log.Warn("Process exit");
+            return runMode == RunMode.AppDomain && domain != null || runMode == RunMode.Process && process != null;
         }
-     
+
+
+        private void StartAppdomain()
+        {
+            var setup = new AppDomainSetup();
+            setup.ApplicationBase = pathToApp;
+
+            setup.ApplicationBase = appCopy.ShadowPath;
+            setup.PrivateBinPath = appCopy.ShadowPath;
+            var potentialConfigFile = executable + ".config";
+            if (File.Exists(Path.Combine(pathToApp, potentialConfigFile)))
+                setup.ConfigurationFile = potentialConfigFile;
+            domain = AppDomain.CreateDomain(executable, new Evidence(), setup);
+            domain.ExecuteAssembly(GetExecutablePath());
+            domain.DomainUnload += AppExited;
+            setup.ApplicationBase = pathToApp;
+
+        }
+        private void StartProcess()
+        {
+            var setup = CreateSetup();
+            process = new Process();
+            process.StartInfo = setup;
+            process.EnableRaisingEvents = true;
+
+            process.Exited += AppExited;
+
+            process.Start();
+
+
+            if (Environment.UserInteractive)
+            {
+                process.ErrorDataReceived +=
+                    (sendingProcess, errorLine) => log.Error(string.Format("[{0}] {1}", process.ProcessName, errorLine.Data));
+                process.OutputDataReceived +=
+                    (sendingProcess, dataLine) => log.Info(string.Format("[{0}] {1}", process.ProcessName, dataLine.Data));
+
+                process.BeginErrorReadLine();
+                process.BeginOutputReadLine();
+            }
+        }
+        void AppExited(object sender, EventArgs e)
+        {
+            log.Warn($"Application exited (Runmode:{runMode}");
+        }
+
 
         private ProcessStartInfo CreateSetup()
         {
             var setup = new ProcessStartInfo();
-            setup.FileName = Path.Combine(appCopy.ShadowPath, executable);
+            setup.FileName = GetExecutablePath();
             setup.WorkingDirectory = appCopy.ShadowPath;
             setup.UseShellExecute = false;
             setup.RedirectStandardOutput = true;
             setup.RedirectStandardError = true;
+
             //setup.RedirectStandardInput = true;
 
             return setup;
         }
 
-        private readonly object processLock = new object();
+        private string GetExecutablePath()
+        {
+            return Path.Combine(appCopy.ShadowPath, executable);
+        }
+
+        private readonly object appLock = new object();
         private void Stop()
         {
-            lock (processLock)
-                if (process != null)
+            lock (appLock)
+                 switch (runMode)
                 {
-                    process.Kill();
-                    process.Close();
-                    process = null;
+                    case RunMode.AppDomain:
+                        AppDomain.Unload(domain);
+                        domain = null;
+                        break;
+                    case RunMode.Process:
+                        if (process != null)
+                        {
+                            process.Kill();
+                            process.Close();
+                            process = null;
+                        }
+                        break;
+                    default:
+                        throw new InvalidOperationException("Unknown runmode");
                 }
+
         }
 
 
